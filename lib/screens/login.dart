242580d416d6f054826aaa1e7853cbf0548a786f
24aeronyx <11211017@student.itk.ac.id>
2024-10-17 02:40:28 +0800

    Create page login

---
+++ lib/screens/login.dart
@@ -0,0 +1,112 @@
+// login_screen.dart
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart'; // Import the custom button
+import 'main_screen.dart'; // Import MainScreen
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      // Handle login logic here
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Logged in as ${_emailController.text}')),
+      );
+
+      // Navigate to MainScreen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(0, 74, 173, 1),
+        title: const Text('Login', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Email TextField
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                controller: _emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              // Password TextField
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                controller: _passwordController,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              // Login Button
+              Container(
+                width: double.infinity,
+                child: CustomButton(
+                  text: 'Login',
+                  onPressed: _login,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              // Navigate to Register
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register'); // Assuming you're using named routes
+                },
+                child: const Text('Don\'t have an account? Register here'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
